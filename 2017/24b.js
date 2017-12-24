de19f48145afd83cdb18e1118f6af6874963ceaa
fmdkdd <fmdkdd@0xc0de.fr>
2017-12-24 13:41:57 +0100

    Faster 24b

--- 2017/24b.js
+++ 2017/24b.js
@@ -1,40 +1,31 @@
 function solve(input) {
   let cmps = input.split('\n').map(l => l.split('/').map(c => parseInt(c, 10)))
 
-  let states = [[0, cmps, []]]
-  let bridges = []
-  let seen = new Set()
+  let states = [[0, cmps, 0, 0]]
+  let len_str = []
 
   while (states.length > 0) {
-    let [port, cmps, bridge] = states.pop()
+    let [port, cmps, str, len] = states.pop()
     for (let i=0; i < cmps.length; ++i) {
       let [a,b] = cmps[i]
       let rst = cmps.slice(0, i).concat(cmps.slice(i+1))
       if (a === port) {
-        states.push([b, rst, bridge.concat([[a,b]])])
+        states.push([b, rst, str + a + b, len + 1])
       } else if (b === port) {
-        states.push([a, rst, bridge.concat([[a,b]])])
-      } else {
-        if (bridge.length > 0) {
-          let key = bridge.map(s => s.join('/')).join('--')
-          if (!seen.has(key)) {
-            bridges.push(bridge)
-            seen.add(key)
-          }
-        }
+        states.push([a, rst, str + a + b, len + 1])
+      } else if (len > 0) {
+        len_str.push([len, str])
       }
     }
   }
 
-  //return bridges.length
-  let strengths = bridges.map(b => [b.length, b.reduce((sum, [a,b]) => sum + a + b, 0)])
   let max_length = 0
-  for (let [l, s] of strengths) {
+  for (let [l, s] of len_str) {
     if (l > max_length) {
       max_length = l
     }
   }
-  let longest = strengths.filter(([l, s]) => l === max_length)
+  let longest = len_str.filter(([l, s]) => l === max_length)
   let max_strength = 0
   for (let [l, s] of longest) {
     if (s > max_strength) {
@@ -52,6 +43,6 @@ console.log(solve(`0/2
 0/1
 10/1
 9/10`))
-//: 31
+//: 19
 
-// Runs in 1min22 on the actual input
+// Runs in 18s on the actual input
